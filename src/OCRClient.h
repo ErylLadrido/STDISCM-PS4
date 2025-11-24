b42cf80d6b53c3e00210c2ede8048e0527b3b138
JoaquinRodriguezph <109897751+JoaquinRodriguezph@users.noreply.github.com>
2025-11-24 12:50:49 +0800

    Add files via upload

--- src/OCRClient.h
+++ src/OCRClient.h
@@ -9,6 +9,8 @@
 #include <memory>
 #include <thread>
 #include <atomic>
+#include <queue>
+#include <mutex>
 
 class OCRClient : public QObject {
     Q_OBJECT
@@ -33,6 +35,7 @@ signals:
 
 private:
     void processResults();
+    void processSendQueue();
 
     std::unique_ptr<ocr::OCRService::Stub> m_stub;
     std::shared_ptr<grpc::Channel> m_channel;
@@ -41,8 +44,13 @@ private:
     std::unique_ptr<grpc::ClientReaderWriter<ocr::ImageRequest, ocr::OCRResult>> m_stream;
 
     std::thread m_readerThread;
+    std::thread m_writerThread;
     std::atomic<bool> m_running;
     std::atomic<bool> m_connected;
+
+    // Queue for images to send
+    std::queue<ocr::ImageRequest> m_sendQueue;
+    std::mutex m_queueMutex;
 
     QString m_serverAddress;
 };
